16425eae965db2a969688d4bd56e249a3376bf56
Saalim Sadman <98661736+SadmanSalim@users.noreply.github.com>
2024-11-10 12:42:48 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
 let checkbox = document.querySelector('input[type="checkbox"]');
 let lightImage = document.querySelector('.light-img'); 
+let bulb = document.querySelector('.lit');
 function turn (){
     if (checkbox.checked) {
-        lightImage.src = "./lightoff.jpg";
+        lightImage.src = "./lightoff.png";
+        lightImage.style.filter = '';
+        bulb.style.display = 'none';
     } else {
-        lightImage.src = "./lighton.jpg";
+        lightImage.src = "./lighton.png";
+        lightImage.style.filter = 'drop-shadow(0px 0px 100px gold)';
+        bulb.style.display = 'block';
+
     }
 }
 checkbox.addEventListener('change', turn);
